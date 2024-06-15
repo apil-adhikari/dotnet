61d5203d8e5a96242e9d579b0b2e4b4863fdef9d
Apil Adhikari <apiladhikari207@gmail.com>
2024-06-15 19:02:52 +0545

    feat: Configure seeding of roles and super admin user
    - Implemented SeedingData class to seed roles ('SUPERADMIN', 'ADMIN', 'STUDENT') if they do not exist in the database.
    - Initialized a super admin user with email 'superadmin@sms.com' and assigned the 'SUPERADMIN' role during application startup.
    - Added Identity services and configured with ApplicationUser and IdentityRole in Program.cs.
    - Integrated role seeding and super admin user initialization into the application startup process.

---
+++ LearningProject/ApilSMS/ApilSMS.Web/Data/SeedingData.cs
@@ -0,0 +1,57 @@
+﻿using ApilSMS.Web.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ApilSMS.Web.Data
+{
+    public class SeedingData
+    {
+        // Method to seed roles and initilize admin user
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            // Get the RoleManager and UserManager from the service provider
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            // Defining the roles we need for our applicaiton
+            string[] roleNames = { "SUPERADMIN", "ADMIN", "STUDENT" };
+
+            // Seeding Roles if they do not exist
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            // Seeding admin user if role doesn't exist
+            if (await userManager.FindByEmailAsync("superadmin@sms.com") == null)
+            {
+
+                var role = roleManager.FindByNameAsync("SUPERADMIN").Result;
+                var superAdminUser = new ApplicationUser
+                {
+                    Email = "superadmin@sms.com",
+                    UserName = "superadmin@sms.com",
+                    FirstName = "Super",
+                    LastName = "Admin",
+                    UserRoleId = role.Id,
+                    EmailConfirmed = true,
+                    IsActive = true,
+                    CreatedDate = DateTime.Now
+                };
+
+                // Creating the super admin with password
+                var result = await userManager.CreateAsync(superAdminUser, "SuperAdmin@123");
+                await userManager.SetLockoutEnabledAsync(superAdminUser, false);
+
+                // Assign the "SUPERADMIN" role to the super admin user
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(superAdminUser, "SUPERADMIN");
+                }
+            }
+        }
+    }
+}
